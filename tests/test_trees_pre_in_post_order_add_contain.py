a7acc55704ea1380cc075cab855153c63d9c7469
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-07-03 20:32:15 +0300

    all test and methodes are made to the code challenge 15

---
+++ tests/test_trees_pre_in_post_order_add_contain.py
@@ -0,0 +1,116 @@
+from trees.tree import *
+import pytest
+import builtins
+
+
+# 1- Can successfully instantiate an empty tree
+def test_instantiate_an_empty_tree():
+    empty = Binary_tree()
+    assert empty.root is None
+
+
+# 2- Can successfully instantiate a tree with a single root node
+def test_instantiate_an_one_node_tree():
+    one = Binary_tree()
+    one.root = Node(50)
+    assert one.root.val == 50
+    assert one.root.right is None
+    assert one.root.left is None
+
+
+# 3- For a Binary Search Tree, can successfully add a left child and right child properly to a node
+def test_add_to_left_and_right():
+    right_left = Binary_Search_Tree()
+    right_left.add(50)
+    assert right_left.root.val == 50
+    right_left.add(80)
+    assert right_left.root.right.val == 80
+    right_left.add(30)
+    assert right_left.root.left.val == 30
+
+
+# 4- Can successfully return a collection from a preorder traversal
+def test_pre_order_traversal(my_tree):
+    real_print = builtins.print
+
+    text = ""
+
+    def mock_print(*args):
+        nonlocal text
+        text += "".join(args) + "\n"
+
+    builtins.print = mock_print
+
+    my_tree.pre_order(my_tree.root)
+    assert text == 'A\nB\nD\nE\nC\nF\n'
+
+    builtins.print = real_print
+
+
+# 5- Can successfully return a collection from an inorder traversal
+def test_in_order_traversal(my_tree):
+    real_print = builtins.print
+
+    text = ""
+
+    def mock_print(*args):
+        nonlocal text
+        text += "".join(args) + "\n"
+
+    builtins.print = mock_print
+
+    my_tree.in_order(my_tree.root)
+    assert text == 'D\nB\nE\nA\nF\nC\n'
+
+    builtins.print = real_print
+
+
+# 6- Can successfully return a collection from a postorder traversal
+def test_post_order_traversal(my_tree):
+    expected = my_tree.post_order(my_tree.root)
+    actual = ['D', 'E', 'B', 'F', 'C', 'A']
+    assert actual == expected
+
+
+# 7- Test the contains method
+def test_contains_true_false(my_search_tree):
+    # True
+    assert my_search_tree.contains(50) is True
+    assert my_search_tree.contains(30) is True
+    assert my_search_tree.contains(60) is True
+    # False
+    assert my_search_tree.contains(1000) is False
+    assert my_search_tree.contains(10) is False
+    assert my_search_tree.contains(46) is False
+
+
+@pytest.fixture
+def my_tree():
+    tree = Binary_tree()
+
+    tree.root = Node("A")
+
+    tree.root.left = Node("B")
+    tree.root.left.left = Node("D")
+    tree.root.left.right = Node("E")
+
+    tree.root.right = Node("C")
+    tree.root.right.left = Node("F")
+
+    return tree
+
+
+@pytest.fixture
+def my_search_tree():
+    tree = Binary_Search_Tree()
+
+    tree.root = Node(50)
+
+    tree.root.left = Node(40)
+    tree.root.left.left = Node(30)
+    tree.root.left.right = Node(45)
+
+    tree.root.right = Node(70)
+    tree.root.right.left = Node(60)
+
+    return tree
---
+++ trees/tree.py
@@ -0,0 +1,152 @@
+import numpy as np
+
+
+# np.array([1,2,3])
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+'''
+Binary Tree
+Create a Binary Tree class
+Define a method for each of the depth first traversals:
+pre order
+in order
+post order which returns an array of the values, ordered appropriately.
+'''
+
+
+class Binary_tree:
+    def __init__(self):
+        self.root = None
+
+    def pre_order(self, root):
+        '''
+            root >> left >> right
+            return: nothing
+        '''
+        print(root.val)
+        if root.left:
+            self.pre_order(root.left)
+        if root.right:
+            self.pre_order(root.right)
+
+    def in_order(self, root):
+        '''
+            left >> root >> right
+            return: nothing
+        '''
+
+        if root.left:
+            self.in_order(root.left)
+        print(root.val)
+        if root.right:
+            self.in_order(root.right)
+
+    def post_order(self, root, arr=[]):
+        '''
+            left >> right >> root
+            return: array with values ordered appropriately.
+        '''
+
+        if root.left:
+            self.post_order(root.left, arr)
+
+        if root.right:
+            self.post_order(root.right, arr)
+
+        arr.append(root.val)
+
+        if root is self.root:
+            return arr
+
+
+class Binary_Search_Tree(Binary_tree):
+
+    def add(self, value):
+        '''
+        :param value:
+        :return: Nothing
+        '''
+        cur = self.root
+        new_node = Node(value)
+
+        if self.root is None:
+            self.root = Node(value)
+            return
+
+        while True:
+
+            if cur.val <= value:
+                if not cur.right:
+                    cur.right = new_node
+                    break
+                else:
+                    cur = cur.right
+                    continue
+
+            if cur.val > value:
+                if not cur.left:
+                    cur.left = new_node
+                    break
+                else:
+                    cur = cur.left
+
+    def contains(self, value):
+        '''
+        :param value:
+        :return: boolean indicating whether or not the value is in the tree at least once.
+        '''
+        cur = self.root
+
+        while cur:
+            if cur.val == value:
+                return True
+            else:
+                if cur.val < value:
+                    cur = cur.right
+                else:
+                    cur = cur.left
+        return False
+
+
+if __name__ == "__main__":
+    tree = Binary_tree()
+
+    tree.root = Node("A")
+
+    tree.root.left = Node("B")
+    tree.root.left.left = Node("D")
+    tree.root.left.right = Node("E")
+
+    tree.root.right = Node("C")
+    tree.root.right.left = Node("F")
+
+    # tree.pre_order(tree.root)
+    # tree.in_order(tree.root)
+    print(tree.post_order(tree.root))
+
+    ###########################################
+
+    ### Binary_Search_Tree
+    print("#" * 50)
+    print('Binary_Search_Tree')
+    search_Tree = Binary_Search_Tree()
+
+    search_Tree.root = Node(50)
+
+    search_Tree.root.left = Node(40)
+    search_Tree.root.left.left = Node(30)
+    search_Tree.root.left.right = Node(45)
+
+    search_Tree.root.right = Node(70)
+    search_Tree.root.right.left = Node(60)
+
+    search_Tree.add(499)
+
+    print(search_Tree.contains(499))
+    print("=" * 10)
+    print(search_Tree.pre_order(search_Tree.root))
