f3f0f41c850bd2678d4e4eb0a4fd9051a1b4e37b
Mohammad99Azim <103434512+Mohammad99Azim@users.noreply.github.com>
2022-07-06 19:30:54 +0300

    refactor: the tree_breadth_first now is a fucntion and more test was added

--- tests/test_breadth_first.py
+++ tests/test_breadth_first.py
@@ -2,24 +2,47 @@ from tree_breadth_first.bredth_first_search import *
 import pytest
 
 
-def test_max_value(my_tree):
-    my_tree.tree_breadth_first() == [50,40,70,30,45,60]
+def test_make_empty_Node():
+    the_tree = My_Tree()
+    the_tree.root = Node(2)
+    the_tree.root.val == 2
+
+def test_breadth_first(my_tree):
+    tree_breadth_first(my_tree) == [50,40,70,30,45,60]
 
 
-def test_max_value_2(my_tree):
-    my_tree.root.right.left.right = Node(100)
-    my_tree.tree_breadth_first() == [50,40,70,30,45,60,100]
+def test_breadth_first_2(tree_two):
+    tree_breadth_first(tree_two) == [50,40,70,30,45,60,100]
+
+def test_breadth_first_3(tree_two):
+    tree_breadth_first(tree_two) == [2, 7, 5, 2, 6, 4, 9, 5, 11]
 
 
-def test_max_raise_exception():
+def test_breadth_first_raise_exception():
     tree = My_Tree()
     with pytest.raises(Exception) as e:
-        tree.tree_breadth_first()
+        tree_breadth_first(tree)
     assert repr(e) == "<ExceptionInfo Exception('The Tree Is Empty !!!') tblen=2>"
+
 
 
 @pytest.fixture
 def my_tree():
+    tree = My_Tree()
+
+    tree.root = Node(50)
+
+    tree.root.left = Node(40)
+    tree.root.left.left = Node(30)
+    tree.root.left.right = Node(45)
+
+    tree.root.right = Node(70)
+    tree.root.right.left = Node(60)
+
+    return tree
+
+@pytest.fixture
+def tree_two():
     tree = My_Tree()
 
     tree.root = Node(50)
--- tree_breadth_first/bredth_first_search.py
+++ tree_breadth_first/bredth_first_search.py
@@ -10,38 +10,21 @@ class My_Tree:
     def __init__(self):
         self.root = None
 
-    def tree_breadth_first(self, root=None):
-        if self.root is None:
-            raise Exception("The Tree Is Empty !!!")
-        if root is None:
-            root = self.root
 
-        queue = []
-        results = []
-        queue.append(root)
-        while queue:
-            if queue[0].left:
-                queue.append(queue[0].left)
+def tree_breadth_first(root):
+    if root.root is None:
+        raise Exception("The Tree Is Empty !!!")
+    root = root.root
+    queue = []
+    results = []
+    queue.append(root)
+    while queue:
+        if queue[0].left:
+            queue.append(queue[0].left)
 
-            if queue[0].right:
-                queue.append(queue[0].right)
+        if queue[0].right:
+            queue.append(queue[0].right)
 
-            results.append(queue[0].val)
-            queue.pop(0)
-        return results
-
-
-if __name__ == "__main__":
-    the_tree = My_Tree()
-
-    the_tree.root = Node(2)
-    the_tree.root.right = Node(5)
-    the_tree.root.right.right = Node(9)
-    the_tree.root.right.left = Node(4)
-
-    the_tree.root.left = Node(7)
-    the_tree.root.left.left = Node(2)
-    the_tree.root.left.right = Node(6)
-    the_tree.root.left.right.left = Node(5)
-    the_tree.root.left.right.right = Node(11)
-    print(the_tree.tree_breadth_first(the_tree.root))
+        results.append(queue[0].val)
+        queue.pop(0)
+    return results
